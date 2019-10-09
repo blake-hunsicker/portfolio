c2a8e32334b17e3860608a158547af17ba73599e
Blake Hunsicker <blakeyhunsicker@gmail.com>
2019-10-08 20:18:29 -0400

    Dunno

---
+++ src/pages/diagram.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Diagram = () => (
+  <Layout>
+    <SEO title="Diagram" />
+    <h1>Diagram</h1>
+    <p>In August, 2019, I launched Diagram News, a blog that covers artificial intelligence. I am the founder, writer, designer and developer.</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default Diagram
---
+++ src/pages/native.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Native = () => (
+  <Layout>
+    <SEO title="Native" />
+    <h1>Native iOS and Android design</h1>
+    <p>In August, 2019, I launched Diagram News, a blog that covers artificial intelligence. I am the founder, writer, designer and developer.</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default Native
